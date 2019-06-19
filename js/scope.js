812979ce3428353b12837f30c488af2b00fa2320
saleemmcpherson <saleemmcpherson@yahoo.com>
2019-06-19 13:13:57 -0400

    added notes about function scope

--- js/scope.js
+++ js/scope.js
@@ -1,3 +1,54 @@
+"use strict"
+
+// *************
+// FUNCTIONS
+// *************
+
+// ======================================================
+// Javascript allows the programmer to define a function 
+// multiple different ways. Generally done by declaring 
+// as a function, giving name, passing params, and code
+// to be done. This particular function can be called  
+// before or after function declaration and output the 
+// same result. Always start with the function keyword.
+// ======================================================
+
+// myFunction(5, 20); 
+
+// function myFunction(num1, num2){
+//     let num3 = num1 + num2;
+//     console.log(num1 + "+" + num2 + "=" + num3);
+// }
+
+// myFunction(5, 20);
+
+// ======================================================
+// Function expressions are declared as a variable and 
+// assigned the properties of a function. This does the 
+// same function as the one above but the property is  
+// changed to that of a variable, so it is treated as a
+// variable and cant be called before it is initialized.
+// ======================================================
+
+
+// // this statement breaks
+// myFunction(5, 20);
+
+// let myFunction = function(num1, num2){
+//     let num3 = num1 + num2;
+//     console.log(num1 + "+" + num2 + "=" + num3)
+// }
+
+// myFunction(5, 20);
+
+
+
+
+
+// **********
+// CLOSURE
+// **********
+
 // ======================================================
 // This code will work because all variables declared 
 // outside of the block scope are accessible without a 
